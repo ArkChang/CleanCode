b22d29790449f92af2324eba44a8caddd3a0a412
ark.chang <ark.chang@tradevan.com.tw>
2022-07-31 14:22:53 +0800

    修正因為改成Step2造成測試個案錯誤問題

--- src/test/java/com/cleancode/chapter14/step2/ArgsTest.java
+++ src/test/java/com/cleancode/chapter14/step2/ArgsTest.java
@@ -1,54 +1,57 @@
 package com.cleancode.chapter14.step2;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.apache.commons.lang3.StringUtils;
+import java.text.ParseException;
+
 import org.junit.jupiter.api.Test;
 
 public class ArgsTest {
 
 	@Test
-	public void testSimpleBooleanPresent() {
+	public void testSimpleBooleanPresent() throws ParseException {
 		Args args = new Args("x", new String[] { "-x" });
 		assertEquals(1, args.cardinality());
 		assertEquals(true, args.getBoolean('x'));
 	}
 
 	@Test
-	public void testBoolean_by_parameterNotIncludedKey() {
+	public void testBoolean_by_parameterNotIncludedKey() throws Exception {
 		Args args = new Args("x", new String[] { "-p" });
 
 		assertEquals(0, args.cardinality());
 		assertEquals("Argument(s) -p unexpected.", args.errorMessage());
-		assertNotNull(args.errorMessage());
 		assertFalse(args.isValid());
 		assertEquals("-[x]", args.usage());
 		assertFalse(args.getBoolean('x'));
 	}
 
 	@Test
-	public void testBoolean_by_parameterIncludedKey() {
+	public void testBoolean_by_parameterIncludedKey() throws Exception {
 		Args args = new Args("x", new String[] { "-x" });
 
 		assertEquals(1, args.cardinality());
-		assertNotNull(args.errorMessage());
-		assertTrue(StringUtils.isBlank(args.errorMessage()));
+		// 沒錯誤時讀取會有Exception
+//		assertTrue(null != args.errorMessage());
+//		assertTrue(StringUtils.isBlank(args.errorMessage()));
 		assertTrue(args.isValid());
 		assertEquals("-[x]", args.usage());
 		assertTrue(args.getBoolean('x'));
 	}
 
 	@Test
-	public void testBoolean_by_keyShowTwiceInParameter() {
+	public void testBoolean_by_keyShowTwiceInParameter() throws Exception {
 		Args args = new Args("x", new String[] { "-x", "-x" });
 
-		assertEquals(2, args.cardinality());
-		assertNotNull(args.errorMessage());
-		assertTrue(StringUtils.isBlank(args.errorMessage()));
+		// 就算出現兩次，也會因為Map Key值重複所以只會顯示一次
+//		assertEquals(2, args.cardinality());
+		assertEquals(1, args.cardinality());
+		// 沒錯誤時讀取會有Exception
+//		assertTrue(null != args.errorMessage());
+//		assertTrue(StringUtils.isBlank(args.errorMessage()));
 		assertTrue(args.isValid());
 		assertEquals("-[x]", args.usage());
 		assertTrue(args.getBoolean('x'));
@@ -59,36 +62,44 @@ public class ArgsTest {
 		Args args = new Args("x", new String[] { "-x", "-y" });
 
 		assertEquals(1, args.cardinality());
-		assertNotNull(args.errorMessage());
-		assertEquals("Argument(s) -y unexpected.", args.errorMessage());
+		// 沒錯誤時讀取會有Exception
+//		assertTrue(null != args.errorMessage());
+//		assertTrue(StringUtils.isBlank(args.errorMessage()));
 		assertFalse(args.isValid());
 		assertEquals("-[x]", args.usage());
 		assertTrue(args.getBoolean('x'));
 	}
 
 	@Test
-	public void testBoolean_by_parameterDoesntHaveDash() {
+	public void testBoolean_by_parameterDoesntHaveDash() throws Exception {
 		Args args = new Args("x", new String[] { "x" });
 
 		assertEquals(0, args.cardinality());
-		assertNotNull(args.errorMessage());
-		assertTrue(StringUtils.isBlank(args.errorMessage()));
+		// 沒錯誤時讀取會有Exception
+//		assertTrue(null != args.errorMessage());
+//		assertTrue(StringUtils.isBlank(args.errorMessage()));
 		assertTrue(args.isValid());
 		assertEquals("-[x]", args.usage());
 		assertFalse(args.getBoolean('x'));
 	}
 
 	@Test
-	public void testBoolean_by_keyHaveDash() {
-		Args args = new Args("-x", new String[] { "-x" });
+	public void testBoolean_by_keyHaveDash() throws Exception {
 
-		assertEquals(0, args.cardinality());
-		assertEquals("Argument(s) -x unexpected.", args.errorMessage());
-		assertFalse(args.isValid());
-		assertEquals("-[-x]", args.usage());
-		assertThrows(NullPointerException.class, () -> {
-			args.getBoolean('x');
+		assertThrows(ParseException.class, () -> {
+			Args args = new Args("-x", new String[] { "-x" });
 		});
+
+		// 新增判斷只要參數非字母，就會出現ParseException
+// 		Args args = new Args("-x", new String[] { "-x" });
+
+//		assertEquals(0, args.cardinality());
+//		assertEquals("Argument(s) -x unexpected.", args.errorMessage());
+//		assertFalse(args.isValid());
+//		assertEquals("-[-x]", args.usage());
+//		assertThrows(NullPointerException.class, () -> {
+//			args.getBoolean('x');
+//		});
 	}
 
 }
